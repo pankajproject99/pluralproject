d128c8a90d5fb9672ca824137bc45dc9b976924c
pankajproject99 <pankajproject99@gmail.com>
2021-10-01 15:30:32 +0530

    15. Hooks: Effect and State 		Import Hook on main page useEffect help in creating sideEffect when state of component changes

--- pluralPracticalStartWithReact/globomantics/src/main-page/index.js
+++ pluralPracticalStartWithReact/globomantics/src/main-page/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./main-page.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -5,11 +6,20 @@ import Header from "./header";
 
 function App() {
   // load data
-  const userName="Pankaj";
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
   return (
     <div className="container">
       <Header
-        subtitle={userName}
+        subtitle={"Providing houses all over the world"}
         title="Some Title"
       />
     </div>
