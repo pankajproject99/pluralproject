af3aebd3895d7324186931470075934c2f2b915c
pankajproject99 <pankajproject99@gmail.com>
2022-02-06 20:31:28 +0530

    project: reactallcomponent: App bar from material UI

--- reactallcomponent/src/App.js
+++ reactallcomponent/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        React All Component
-      </header>
+      <Navbar></Navbar>
     </div>
   );
 }
