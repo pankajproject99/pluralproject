42703a9e657283c42244e0893259af44300595f4
pankajproject99 <pankajproject99@gmail.com>
2022-12-04 17:10:11 +0530

    reactallcomponent: pending commit

--- reactallcomponent/src/components/Navbar/Navbar.js
+++ reactallcomponent/src/components/Navbar/Navbar.js
@@ -10,11 +10,12 @@ import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import { mainNavbarListItems } from "../consts/navbarItems";
+import { useParams, useNavigate } from "react-router";
 
 const Navbar = () => {
   const drawerWidth = 240;
+  const navigate = useNavigate();
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -40,7 +41,7 @@ const Navbar = () => {
           flexShrink: 0,
           "& .MuiDrawer-paper": {
             width: drawerWidth,
-            boxSizing: "border-box",
+            boxSizing: "border-box" 
           },
         }}
         // always available
@@ -51,65 +52,18 @@ const Navbar = () => {
         <Toolbar />
 
         <Divider />
+        {/* Get list of items from constant as array, array has 2 element, map is key and index */}
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
+          {mainNavbarListItems.map((text, index) => (
+            <ListItem button key={text.id}>
               <ListItemIcon>
-                {/* Alternate icon, for first Inbox and for second mailBox */}
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {text.icon}
               </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={text.label} />
             </ListItem>
           ))}
         </List>
       </Drawer>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
-      >
-        <Toolbar />
-        {/* Readymate formating for text, here display as paragraph */}
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box>
     </Box>
   );
 };
---
+++ reactallcomponent/src/components/consts/navbarItems.js
@@ -0,0 +1,26 @@
+import PeopleIcon from '@mui/icons-material/People';
+
+export const mainNavbarListItems = [
+    {
+        id:0,
+        icon:<PeopleIcon/>,
+        label:'Autentication',
+        route:'authentication',
+    },
+    {
+        id:1,
+        icon:'',
+        label:'Database',
+        route:'database',
+    },{
+        id:3,
+        icon:'',
+        label:'Publication',
+        route:'publication',
+    },{
+        id:4,
+        icon:'',
+        label:'Storage',
+        route:'storage',
+    },
+]
